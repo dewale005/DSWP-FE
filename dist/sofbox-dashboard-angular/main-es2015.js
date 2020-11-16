08b6712748a8ba3ae5edb6f2aaac1c79152f088e
Adewale Olagunju <adewale@Adewales-MacBook-Pro.local>
2020-11-16 14:53:23 +0100

    test version

--- dist/sofbox-dashboard-angular/main-es2015.js
+++ dist/sofbox-dashboard-angular/main-es2015.js
@@ -3829,9 +3829,9 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    // apiEndpoint: 'http://3.138.110.220:3200',
+    apiEndpoint: 'http://3.138.110.220:3200',
     // apiEndpoint: 'http://3.138.110.220:3100',
-    apiEndpoint: 'http://127.0.0.1:3200',
+    // apiEndpoint: 'http://127.0.0.1:3200',
     mapbox: {
         accessToken: 'pk.eyJ1IjoiZ29sZGVubWFjZWRldiIsImEiOiJjazBnYnFxb3AwNWdqM2JuNWRwazZxMWY0In0._I3zpcM9QTHNqcXWjWpD6Q',
         mapStyle: 'mapbox://styles/goldenmacedev/ck49vpeap08b61cnubglcag7l'
--- dist/sofbox-dashboard-angular/main-es5.js
+++ dist/sofbox-dashboard-angular/main-es5.js
@@ -5977,9 +5977,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
     var environment = {
       production: false,
-      // apiEndpoint: 'http://3.138.110.220:3200',
+      apiEndpoint: 'http://3.138.110.220:3200',
       // apiEndpoint: 'http://3.138.110.220:3100',
-      apiEndpoint: 'http://127.0.0.1:3200',
+      // apiEndpoint: 'http://127.0.0.1:3200',
       mapbox: {
         accessToken: 'pk.eyJ1IjoiZ29sZGVubWFjZWRldiIsImEiOiJjazBnYnFxb3AwNWdqM2JuNWRwazZxMWY0In0._I3zpcM9QTHNqcXWjWpD6Q',
         mapStyle: 'mapbox://styles/goldenmacedev/ck49vpeap08b61cnubglcag7l'
