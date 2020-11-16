c5a584f51f9cdc4a4a6ec07008d4d329bff02f6d
Adewale Olagunju <adewale@Adewales-MacBook-Pro.local>
2020-11-16 06:41:13 +0100

    latest changes

--- dist/sofbox-dashboard-angular/main-es5.js
+++ dist/sofbox-dashboard-angular/main-es5.js
@@ -5876,8 +5876,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
     var environment = {
       production: false,
-      apiEndpoint: 'http://127.0.0.1:3200',
-      // apiEndpoint: 'http://3.138.110.220:3100',
+      // apiEndpoint: 'http://3.138.110.220:3200',
+      apiEndpoint: 'http://3.138.110.220:3100',
       // apiEndpoint: 'http://localhost:3000',
       mapbox: {
         accessToken: 'pk.eyJ1IjoiZ29sZGVubWFjZWRldiIsImEiOiJjazBnYnFxb3AwNWdqM2JuNWRwazZxMWY0In0._I3zpcM9QTHNqcXWjWpD6Q',
