371a612ea8a3237c314fe2d8da8c7cf239560691
Adewale Olagunju <adewale@Adewales-MacBook-Pro.local>
2020-12-21 15:27:00 +0100

    bug fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiEndpoint: 'https://shop.drugstoc.com:3200',
-  apiEndpoint: 'https://localhost:3200',
+  apiEndpoint: 'https://shop.drugstoc.com:3200',
+  // apiEndpoint: 'https://localhost:3200',
   // apiEndpoint: 'https://localhost/api',
   // apiEndpoint: 'http://3.129.99.242:3200',
   mapbox: {
